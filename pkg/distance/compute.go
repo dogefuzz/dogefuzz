aab062239b9277a8a6a5fbc9d970a837f529185b
Ismael Medeiros <ismaelcmedeiros@gmail.com>
2023-01-15 02:05:35 -0300

    feat(distance): add ComputeDeltaMinDistance method implementation

--- pkg/distance/compute.go
+++ pkg/distance/compute.go
@@ -3,8 +3,13 @@ package distance
 import "github.com/dogefuzz/dogefuzz/pkg/common"
 
 func ComputeDeltaMinDistance(distanceMap common.DistanceMap, instructionsExecutedInTransaction []string, instructionsExecutedInTask []string) int64 {
-	// TODO: Add logic to compute the delta min distance following an exploration strategy
-	return 0
+	currentMinDistance := ComputeMinDistance(distanceMap, instructionsExecutedInTask)
+	mergedInstructions := common.MergeSortedSlices(instructionsExecutedInTask, instructionsExecutedInTransaction)
+	newMinDistance := ComputeMinDistance(distanceMap, mergedInstructions)
+	if newMinDistance > currentMinDistance {
+		return 0
+	}
+	return currentMinDistance - newMinDistance
 }
 
 func ComputeMinDistance(distanceMap common.DistanceMap, instructions []string) int64 {
